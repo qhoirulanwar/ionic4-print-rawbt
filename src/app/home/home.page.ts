166bc0aa19331c70759d2c978dc062a396eb9539
Qhoirul Anwar <qhoir.anwar@gmail.com>
2019-08-09 01:06:50 +0700

    setup code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import EscPosEncoder from 'esc-pos-encoder-ionic';
 
+// declare var Socket: any;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +10,41 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  encoder = new EscPosEncoder();
+
+  // https://github.com/Ans0n-Ti0/EscPosEncoder#readme
+  result = this.encoder.initialize()
+    .text('The quick brown fox jumps over the lazy dog')
+    .newline()
+    .align('center')
+    .qrcode('12345678')
+
+    .align('right')
+    .line('aligned the right')
+    .align('center')
+    .line('This line is centered')
+    .align('left')
+    .line('aligned the left')
+
+    .newline()
+    .encode();
+
+  // Convert uint8 array to base64 encoded string
+  // https://stackoverflow.com/questions/12710001/how-to-convert-uint8-array-to-base64-encoded-string
+  u8 = new Uint8Array(this.result);
+  decoder = new TextDecoder('utf8');
+  b64encoded = btoa(this.decoder.decode(this.u8));
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  sanitize() {
+    const urlintent: string = 'rawbt:base64,'
+    return this.sanitizer.bypassSecurityTrustUrl(urlintent + this.b64encoded);
+  };
+
+  openWindow() {
+    const urlintent: string = 'rawbt:base64,'
+    window.open(urlintent + this.b64encoded)
+  }
 
 }
